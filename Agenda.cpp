0085fdb986411b5bfe97eba70f0b971c39b5d40b
Apaza Lipa Josue Paul <75494394@continental.edu.pe>
2023-04-09 15:24:58 -0500

    correción de errores

--- Agenda.cpp
+++ Agenda.cpp
@@ -120,7 +120,7 @@ int main(int argc, char *argv[]) {
 									}
 								} while (op!=1 && op!=2 && op!=3 && op!=4 && op!=5 && op!=6);
 								
-							} while (op!=6);
+							} while (op!=1 && op!=2 && op!=3 && op!=4 && op!=5 && op!=6);
 							switch (op)
 							{
 							case 1:
